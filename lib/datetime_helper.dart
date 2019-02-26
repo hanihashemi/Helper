fe58ac186c4f166fd0383fd41e616c8d51f8d6f5
Hani Hashemi <jhanihashemi@gmail.com>
2019-02-27 01:15:14 +0300

    Add date formatter and change Datetime to DateTime

--- lib/datetime_helper.dart
+++ lib/datetime_helper.dart
@@ -1,4 +1,6 @@
-class DatetimeHelper {
+import 'package:intl/intl.dart';
+
+class DateTimeHelper {
   DateTime convert(String strDatetime) {
     return DateTime.parse(strDatetime);
   }
@@ -12,5 +14,12 @@ class DatetimeHelper {
   int toAgeTimestampMilliseconds(int birthday, [int now]) {
     return toAge(DateTime.fromMillisecondsSinceEpoch(birthday),
         DateTime.fromMillisecondsSinceEpoch(now));
+  }
+
+  String dateToString(int timestamp, [String format = 'dd-MM-yyyy']) {
+    if (timestamp == null || timestamp == 0) return '-';
+    var dateTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    var dateFormat = DateFormat(format);
+    return dateFormat.format(dateTime);
   }
 }
--- test/datetime_helper_test.dart
+++ test/datetime_helper_test.dart
@@ -3,7 +3,7 @@ import 'package:helper/datetime_helper.dart';
 
 void main() {
   test('convert, Long String to DateTime', () {
-    DatetimeHelper datetimeHelper = DatetimeHelper();
+    DateTimeHelper datetimeHelper = DateTimeHelper();
     expect(
         datetimeHelper
             .convert('1974-03-20 00:00:00.000')
@@ -12,37 +12,44 @@ void main() {
   });
 
   test('convert, Short String to DateTime', () {
-    DatetimeHelper datetimeHelper = DatetimeHelper();
+    DateTimeHelper datetimeHelper = DateTimeHelper();
     expect(datetimeHelper.convert('2019-02-06').millisecondsSinceEpoch,
         1549400400000);
   });
 
   test('calculate age, scenario 1', () {
-    DatetimeHelper datetimeHelper = DatetimeHelper();
+    DateTimeHelper datetimeHelper = DateTimeHelper();
     int age =
         datetimeHelper.toAge(DateTime(1989, 2, 14), DateTime(2018, 12, 25));
     expect(age, 29);
   });
 
   test('calculate age, scenario 2', () {
-    DatetimeHelper datetimeHelper = DatetimeHelper();
+    DateTimeHelper datetimeHelper = DateTimeHelper();
     int age =
         datetimeHelper.toAge(DateTime(1989, 2, 14), DateTime(2019, 1, 23));
     expect(age, 29);
   });
 
   test('calculate age, scenario 3', () {
-    DatetimeHelper datetimeHelper = DatetimeHelper();
+    DateTimeHelper datetimeHelper = DateTimeHelper();
     int age =
         datetimeHelper.toAge(DateTime(1989, 2, 14), DateTime(2019, 2, 23));
     expect(age, 30);
   });
 
   test('calculate age, scenario 4', () {
-    DatetimeHelper datetimeHelper = DatetimeHelper();
+    DateTimeHelper datetimeHelper = DateTimeHelper();
     int age = datetimeHelper.toAgeTimestampMilliseconds(
         DateTime(1989, 2, 14).millisecondsSinceEpoch,
         DateTime(2019, 2, 23).millisecondsSinceEpoch);
     expect(age, 30);
+  });
+
+  test('convert timestamp to date', (){
+    DateTimeHelper datetimeHelper = DateTimeHelper();
+    int timestamp = DateTime(2019, 2, 23).millisecondsSinceEpoch;
+
+    expect(datetimeHelper.dateToString(timestamp), '23-02-2019');
   });
 }
